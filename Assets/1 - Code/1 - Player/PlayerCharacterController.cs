776ffcaf0b7649aebb6b173387dbdd46ae520fce
Justin Gaborit <57820526+Justin-Gaborit@users.noreply.github.com>
2021-02-21 20:27:20 -0500

    Update 2/21/2021
    - Added new movement script

--- Assets/1 - Code/1 - Player/PlayerCharacterController.cs
+++ Assets/1 - Code/1 - Player/PlayerCharacterController.cs
@@ -3,33 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacterController : MonoBehaviour
-{
-    public Rigidbody _PlayerRigidbody;              //gives the player a rigidbody
-
-    public float _PlayerGravity;                    //downward force applied to the player   
+{   
     public float _PlayerForwardSpeed;               //forward speed applied to the player
     public float _PlayerHorizontalSpeed;            //horizontal movement speed applied to the player
     public float _PlayerHorizontalSpeedMaximum;     //maximum horizontal speed the player can move at
-    public float _PlayerJumpForce;                  //upward force applied to the player 
 
     public bool _PlayerisGrounded;                  //returns if the player is on the ground
-    public bool _PlayerJumpHeightMet;               //returns if the player has reached maximum jump height
     public bool _PlayerisJumping;                   //returns if the player is in the air
 
     // Start is called before the first frame update
     void Start()
     {
-        _PlayerRigidbody = GetComponent<Rigidbody>();           //assigns players rigidbody to "_PlayerRigidbody" variable
+
         _PlayerisGrounded = true;                               //player is grounded by default
-        _PlayerJumpHeightMet = false;                           //player has not met maximum jumpheight by default
         _PlayerHorizontalSpeedMaximum = _PlayerHorizontalSpeed; //this is done to limit maximum horizontal travel speed
-        _PlayerisJumping = false;                               //player is not jumping by default
-        _PlayerForwardSpeed = 0;                                //THIS DEFINES THE SPEED THE PLAYER MOVES AT
+        _PlayerisJumping = false;                               //player is not jumping by default                               
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 0, -_PlayerHorizontalSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, 0, _PlayerHorizontalSpeed * Time.deltaTime);
+        }
+        else 
+        {
+            transform.Rotate(0, 0, 0);
+        }
+
+        transform.position += new Vector3(0,0, _PlayerForwardSpeed * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
     }
 }
---
+++ Assets/1 - Code/1 - Player/PlayerOrientationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOrientationController : MonoBehaviour
+{
+    public GameObject PlayerObject;
+    public float PlayerRotationSpeed;
+    Vector3 PlayerObjectCenter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRotationSpeed = PlayerRotationSpeed + Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit PlayerOBJRaycastHitInfo;
+
+        Physics.Raycast(transform.position, -transform.up, out PlayerOBJRaycastHitInfo, Mathf.Infinity);
+        Debug.DrawRay(transform.position, -transform.up * Mathf.Infinity, Color.red);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, PlayerOBJRaycastHitInfo.transform.localRotation, PlayerRotationSpeed);
+    }
+}
